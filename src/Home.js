b8a66bde187f17ce786b86113283e7f0cefc8398
animevn <haanhth21@gmail.com>
2020-03-30 17:17:51 +0700

    ok now

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import "bootstrap/js/dist/modal"
+const base = {initial: "Waiting", cancel: "Cancel", ok: "OK"};
 
 function Home() {
+
+  const [message, setMessage] = useState(base.initial);
+
+  function onLaunchClick(event) {
+    event.preventDefault();
+    setMessage(base.initial);
+  }
+
+  function onOkClick(event) {
+    event.preventDefault();
+    setMessage(base.ok);
+  }
+
+  function onCancelClick(event) {
+    event.preventDefault();
+    setMessage(base.cancel);
+  }
 
   return (
     <div className="container mt-5">
 
       <div>
-        <h2 className="text-center mb-5">Text here</h2>
+        <h2 className="text-center mb-5">{message}</h2>
       </div>
 
       <div className="text-center">
-        <button className="btn btn-success" data-toggle="modal" data-target="#modal">
+        <button className="btn btn-success" data-toggle="modal" data-target="#modal"
+                onClick={onLaunchClick}>
           Launch demo modal
         </button>
       </div>
@@ -33,11 +52,13 @@ function Home() {
             </div>
 
             <div className="modal-footer d-flex justify-content-around">
-              <button type="button" className="btn btn-danger w-25 px-0" data-dismiss="modal">
+              <button type="button" className="btn btn-danger w-25 px-0" data-dismiss="modal"
+                      onClick={onCancelClick}>
                 Cancel
               </button>
 
-              <button className="btn btn-success w-25" data-dismiss="modal">
+              <button className="btn btn-success w-25" data-dismiss="modal"
+                      onClick={onOkClick}>
                 OK
               </button>
             </div>
