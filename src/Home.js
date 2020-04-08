08cfe2e392a7ace464ee24e5f9b4fa788811e514
animevn <haanhth21@gmail.com>
2020-04-08 23:25:03 +0700

    redesign

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React, {useState} from "react";
-import "bootstrap/js/dist/modal"
 const base = {initial: "Waiting", cancel: "Cancel", ok: "OK"};
 
 function Home() {
@@ -35,37 +34,7 @@ function Home() {
         </button>
       </div>
 
-      <div className="modal fade" id="modal">
-        <div className="modal-dialog modal-sm modal-dialog-centered">
-          <div className="modal-content">
 
-            <div className="modal-body d-flex flex-column pt-1">
-              <div className="container d-flex flex-row justify-content-between px-0 mb-3">
-                <h5 className="modal-title">Your choice</h5>
-                <button className="close" data-dismiss="modal">
-                  <span className="float-right" aria-hidden="true">&times;</span>
-                </button>
-              </div>
-
-              <h6 className="text-center">Choose wisely</h6>
-
-            </div>
-
-            <div className="modal-footer d-flex justify-content-around">
-              <button type="button" className="btn btn-danger w-25 px-0" data-dismiss="modal"
-                      onClick={onCancelClick}>
-                Cancel
-              </button>
-
-              <button className="btn btn-success w-25" data-dismiss="modal"
-                      onClick={onOkClick}>
-                OK
-              </button>
-            </div>
-
-          </div>
-        </div>
-      </div>
     </div>
   )
 }
